2b55d309a50296b4322fde1a3bd1c6b8af495e24
Vladimirs V <vbossmanv@gmail.com>
2017-01-18 10:47:10 +0100

    18th jan

--- day 5/gulpfile.js
+++ day 5/gulpfile.js
@@ -1,11 +1,20 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 
-gulp.task('default', function(){
-    browserSync.init({
-        server: {
-            baseDir: './'
-        },
-        files: ['*.css', '*.html']
-    });
-});
+gulp.task('default', 
+
+    function () {
+        browserSync.init({
+            proxy: {
+                target: "http://www.bootcamp.local",
+            },
+            open: 'external',
+            host: 'www.bootcamp.local',
+            port: 3000;
+            files: ['**/*.css', '**/*.html'
+            ]
+        });
+    }
+);
+
+var browserSync = require('browser-sync').create();
