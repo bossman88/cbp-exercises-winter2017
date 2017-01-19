e3548ca0e0b529461f7b1c7e0c4f83053a17eb12
Vladimirs V <vbossmanv@gmail.com>
2017-01-19 09:42:05 +0100

    tiny change

---
+++ w2d3/js/script.js
@@ -0,0 +1,15 @@
+
+
+
+
+var value;
+var type;
+
+value ='123 Times Square';
+type = typeof value;
+console.log(type);
+
+
+var message_element = document.getElementById('message');
+console.log(message_element);
+console.log(typeof message_element);
