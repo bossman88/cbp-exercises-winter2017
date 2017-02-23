da68d5bb77f1d16f3483911cc30739a9c07de685
Vladimirs Vengrevics <vbossmanv@gmail.com>
2017-02-23 16:26:46 +0100

    day 34 jquery application todo list

---
+++ Day34/lectures/jquery/examples/eventlistener.js
@@ -0,0 +1,13 @@
+$('#btn1').click(function(){
+       
+        $element.text('clicked btn 1');
+
+    })
+
+     $('#btn2').click(function(){
+        $element.text('clicked btn 2');
+    })
+
+     $('#btn3').click(function(){
+        $element.text('clicked btn 3');
+    })
